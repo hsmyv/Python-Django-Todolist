b9d404fba7971fdcc49c7b8eafffb2170ba6233a
Hasan <hsmusayev@gmail.com>
2024-07-18 18:13:48 +0400

    added admin, forms, models and pass data to view

--- tasks/views.py
+++ tasks/views.py
@@ -2,5 +2,17 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import Task
+
+from .forms import TaskForm
+
 def index(request):
-    return HttpResponse('Hello World!')
+    
+    form = TaskForm()
+
+    tasks = Task.objects.all()
+
+    context = {'tasks' : tasks, 'TaskForm' : form}
+
+
+    return render(request,'tasks.html', context)
