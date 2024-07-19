f9f1079765368bcc0f010a19743c9a8db6909dac
Hasan <hsmusayev@gmail.com>
2024-07-19 16:30:07 +0400

    added update and delete task

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name=""),
+
+    path('update-task/<str:pk>/', views.updateTask, name="update-task"),
+
+    path('delete-task/<str:pk>/', views.deleteTask, name="delete-task"),
+
 ]
